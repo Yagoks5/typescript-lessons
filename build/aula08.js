835f287e7ec40e904c36de016144cf0d2682bfa0
Yago <yago.ks5@gmail.com>
2025-02-20 18:23:17 -0300

    aula 08.ts

---
+++ build/aula08.js
@@ -0,0 +1,17 @@
+"use strict";
+let dados = {
+    nome: "Tiltes",
+    idade: 24,
+    status: "A",
+    ola: () => {
+        console.log("Oi");
+    },
+    info: (p) => {
+        console.log(p);
+    },
+};
+console.log(typeof dados);
+console.log(dados);
+console.log(dados.idade);
+dados.ola();
+dados.info("Testando =D");
---
+++ src/aula08.ts
@@ -0,0 +1,18 @@
+let dados = {
+  nome: "Tiltes",
+  idade: 24,
+  status: "A",
+  ola: () => {
+    console.log("Oi");
+  },
+  info: (p: string) => {
+    console.log(p);
+  },
+};
+
+console.log(typeof dados);
+console.log(dados);
+console.log(dados.idade);
+dados.ola();
+
+dados.info("Testando =D");
