7acfb7c0b1628f10d294d6672ce71caeab3e1c86
Yago <yago.ks5@gmail.com>
2025-02-26 17:40:40 -0300

    aula27.ts

---
+++ build/aula27.js
@@ -0,0 +1,15 @@
+"use strict";
+let curso1;
+let curso2;
+curso1 = {
+    titulo: "Typescript",
+    descricao: "Curso typescript",
+    aulas: 35,
+    maxAlunos: 20,
+};
+curso2 = {
+    titulo: "C#",
+    descricao: "Curso C#",
+    aulas: 100,
+    maxAlunos: 20,
+};
---
+++ src/aula27.ts
@@ -0,0 +1,22 @@
+interface curso {
+  titulo: string;
+  descricao: string;
+  aulas: number;
+  maxAlunos: number;
+}
+
+let curso1: curso;
+let curso2: curso;
+
+curso1 = {
+  titulo: "Typescript",
+  descricao: "Curso typescript",
+  aulas: 35,
+  maxAlunos: 20,
+};
+curso2 = {
+  titulo: "C#",
+  descricao: "Curso C#",
+  aulas: 100,
+  maxAlunos: 20,
+};
